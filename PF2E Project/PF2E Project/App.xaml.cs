c88d4bd3a2c9a713f344c1781a37ddcf7642d74c
MichaelRdot <mruss62@lsu.edu>
2025-03-24 23:21:10 -0500

    Started

---
+++ PF2E Project/PF2E Project/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace PF2E_Project
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
